7d93906cf9df28ff237e3b353906de3b69925b72
Jiří Beneš <jiri.benes@svetelnareseni.cz>
2024-01-24 23:26:34 +0100

    přidáno vytvoření uživatele jb a modul system.nix

--- flake.nix
+++ flake.nix
@@ -16,6 +16,7 @@
         modules = [
           ./configuration.nix
           ./vpsadminos.nix
+          ./modules/system.nix
         ];
       };    
     };
---
+++ modules/system.nix
@@ -0,0 +1,11 @@
+{ pkgs, lib, ... }:
+{
+    users.users.jb = {
+    isNormalUser = true;
+    description = "JB";
+    extraGroups = ["networkmanager" "wheel"];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMq0tiV0y8TuAPUE8uHWWOBtjehzc4rXuywi5TziuLMp jb@nixos"
+    ];
+  };
+}
