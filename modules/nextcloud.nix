06d53402c47cb19c88c446351d3bd7f320339c54
jiribenes88 <55151455+jiribenes88@users.noreply.github.com>
2024-03-10 13:47:28 +0100

    Update nextcloud.nix - nastavení https

--- modules/nextcloud.nix
+++ modules/nextcloud.nix
@@ -5,6 +5,7 @@
     enable = true;
     package = pkgs.nextcloud28;
     hostName = "localhost";
+    https = true;
     config.extraTrustedDomains = [ "37.205.13.132" ];
     config.adminuser = "JiriBenes";
     config.adminpassFile = "/etc/nextcloud-admin-pass";
