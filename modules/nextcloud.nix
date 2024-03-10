04238096f4eef6ba0ca4e9f2749793601dbc2c33
jiribenes88 <55151455+jiribenes88@users.noreply.github.com>
2024-03-10 13:13:48 +0100

    Update nextcloud.nix - chybějící středník

--- modules/nextcloud.nix
+++ modules/nextcloud.nix
@@ -5,7 +5,7 @@
     enable = true;
     package = pkgs.nextcloud28;
     hostName = "37.205.13.132";
-    config.adminuser = "JiriBenes"
+    config.adminuser = "JiriBenes";
     config.adminpassFile = "/etc/nextcloud-admin-pass";
   };
 }
