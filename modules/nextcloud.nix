e7eba900ca0302da5c4ad2d9b45b7e5de7edd2a7
Jiří Beneš <jiri.benes@svetelnareseni.cz>
2024-03-10 12:47:10 +0100

    uprava nextcloud.nix

--- modules/nextcloud.nix
+++ modules/nextcloud.nix
@@ -1,8 +1,11 @@
 { pkgs, lib, ... }:
 {
+  environment.etc."nextcloud-admin-pass".text = "PWD";
   services.nextcloud = {
     enable = true;
+    package = pkgs.nextcloud28;
     hostName = "localhost";
     config.adminuser = "JiriBenes"
+    config.adminpassFile = "/etc/nextcloud-admin-pass";
   };
 }
