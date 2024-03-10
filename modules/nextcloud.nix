c2c29680f742d55168ab0a75d583a53e2d9543aa
jiribenes88 <55151455+jiribenes88@users.noreply.github.com>
2024-03-10 13:34:12 +0100

    Update nextcloud.nix - přidány apps a funckce extratrusteddomains

--- modules/nextcloud.nix
+++ modules/nextcloud.nix
@@ -4,8 +4,16 @@
   services.nextcloud = {
     enable = true;
     package = pkgs.nextcloud28;
-    hostName = "localhost";
+    # hostName = "localhost";
+    config.extraTrustedDomains = [ 37.205.13.132 ];
     config.adminuser = "JiriBenes";
     config.adminpassFile = "/etc/nextcloud-admin-pass";
+    
+    # Instead of using pkgs.nextcloud28Packages.apps,
+    # we'll reference the package version specified abov
+    extraApps = {
+      inherit (config.services.nextcloud.package.packages.apps) contacts calendar;
+    };
+    extraAppsEnable = true;
   };
 }
