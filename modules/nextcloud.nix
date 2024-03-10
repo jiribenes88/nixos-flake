78d57296fff83656ed873c2ce81511e37bc25998
jiribenes88 <55151455+jiribenes88@users.noreply.github.com>
2024-03-10 14:15:10 +0100

    Update nextcloud.nix - nastavení defaultní předvolby telefonu, max upload size 5G, a redis cachovaní

--- modules/nextcloud.nix
+++ modules/nextcloud.nix
@@ -7,6 +7,9 @@
     hostName = "localhost";
     config.extraTrustedDomains = [ "37.205.13.132" ];
     config.adminuser = "JiriBenes";
-    config.adminpassFile = "/etc/nextcloud-admin-pass";    
+    config.adminpassFile = "/etc/nextcloud-admin-pass";   
+    config.defaultPhoneRegion = "CZ";                           # nastavení defaultní telefonní předvolby
+    maxUploadSize = "5G";                                       # nastavení maximální velikosti uploadovaného souboru
+    configureRedis = true;                                      # cachování - rychlejší načítání webu
   };
 }
